878c0a76438029ba7ec59e0a8babb0a9c7440f9e
ralphrass <ralphrass@gmail.com>
2017-01-05 14:03:13 -0200

    ajustes

--- 2-3/similaridades.py
+++ 2-3/similaridades.py
@@ -15,8 +15,13 @@ peso = df.loc[:, 'Peso']
 x = [3, 2, 0, 5, 0, 0, 0, 2, 0, 0]
 y = [1, 0, 0, 0, 0, 0, 0, 1, 0, 2]
 
+x = altura
+y = peso
+
 
 print np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
+
+print np.sum(np.cov(x, y))
 
 print np.sum(np.cov(x, y)) / (np.std(x)**2 * np.std(y)**2)
 
